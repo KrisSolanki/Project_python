26825c69ff6d60bfddf7ec6de362e5348be24c57
KrisSolanki <krissolanki28@gmail.com>
2024-07-09 19:21:38 +0530

    Exercise 3: Adding Dynamic Content datetime

--- pyDjango/website/home/views.py
+++ pyDjango/website/home/views.py
@@ -1,4 +1,10 @@
+from datetime import datetime
 from django.shortcuts import render
+
 def index(request):
-    return render(request,"index.html")
+    current_datetime = datetime.now()
+    context = {
+        'current_datetime': current_datetime,
+    }
+    return render(request,"index.html",context)
 # Create your views here.
